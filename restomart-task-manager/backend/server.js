4c66a05e7c5cd69c6ad68f47feb9240b5f089592
PRITHIVI RAJ <rajprithivi099@gmail.com>
2025-06-20 03:47:37 +0530

    Update server.js

--- restomart-task-manager/backend/server.js
+++ restomart-task-manager/backend/server.js
@@ -7,7 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 5173;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://restomartprithivi.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
